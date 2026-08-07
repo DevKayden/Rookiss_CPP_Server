c6e248d506b69c4dbeaf772db3d77ea6d658950e
Kayden <wnstjd2754@gmail.com>
2026-08-07 23:47:23 +0900

    finished until 39. 소켓프로그래밍 기초#2

--- CPP_Server/Server/DummyClient/DummyClient.cpp
+++ CPP_Server/Server/DummyClient/DummyClient.cpp
@@ -1,8 +1,81 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+#include <WinSock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
 int main()
 {
-    HelloWorld();
+    //WinSock 라이브러리를 사용하려면, 라이브러리 초기화를 해줘야한다.
+    /*
+        시작에는 WSAStartup()을 호출해서 초기화
+        두번째 인자인 WSAData에 관련 정보가 들어가긴 하는데, 쓸 일은 없다고 함.
+        성공하면 0을 반환, 실패하면 0이 아닌 값을 반환.
+        
+        WSACleanup()은 마지막에 호출해줘야한다. 근데 호출안한다고 크래시가 나거나 큰 문제는 생기지 않음.
+        그냥 표준에서 호출하라니까 호출하는거.
+    */
+    // WinSock 초기화 (ws2_32 라이브러리 초기화)
+    // 관련 정보가 wsaData에 채워짐
+    WSAData wasData;
+    if (::WSAStartup(MAKEWORD(2, 2), &wasData) != 0)
+        return 0;
+
+    
+    // ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
+    // type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+    // protocol : 0
+    // return : descriptor
+    SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+    // SOCKET 타입 자체가 Int형이다. 해당 번호에 해당하는 소켓을 이용하라고 OS에 요청하는거.
+
+    if (clientSocket == INVALID_SOCKET) // Socket생성 실패시
+    {
+        int32 errCode = ::WSAGetLastError();
+        cout << "Socket ErrorCode : " << errCode << endl;
+        return 0;
+    }
+
+
+    // 연결할 목적지 설정 (IP주소 + 포트)
+    SOCKADDR_IN serverAddr; //IPv4
+    ::memset(&serverAddr, 0, sizeof(serverAddr)); // SOCKADDR_IN 구조체변수 값을 0으로 초기화
+    serverAddr.sin_family = AF_INET;
+    //serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); << deprecated 오래된거라 사용 권장 x 대신 아래코드로 대체
+    ::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); // 127.0.0.1 이 루프백 주소라서 우린 한 컴퓨터에서 클라-서버로 테스트 하니까
+    serverAddr.sin_port = ::htons(7777); // port설정. 서버쪽에서 열어놓은 포트 번호로 지정되어야함. 
+    
+    // htons : host to network short -> host에서 network방식의 Endian으로 맞춰주는 함수이다.
+    // Big-Endian vs Little-Endian 방식이 다를 수 있어서 이 함수를 통해서 포트번호를 넣어주는 것이다.
+    // network에서는 Big-Endian을 사용하기 때문에 거기에 맞춰주는 듯.
+
+
+    if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+    {
+        int32 errCode = ::WSAGetLastError();
+        cout << "Connect ErrorCode : " << errCode << endl;
+        return 0;
+    }
+
+    /*----------------------------------------------------------------------
+        여기까지 왔으면 연결이 성공함. 이제부터 데이터 송수신이 가능하다. 
+    ----------------------------------------------------------------------*/
+
+    cout << "Connected To Server!" << endl;
+
+    while (true)
+    {
+        //TODO
+
+        this_thread::sleep_for(1s);
+    }
+    
+    // 소켓 리소스 반환
+    ::closesocket(clientSocket);
+
+    // WinSock 종료
+    ::WSACleanup();
 }
 
--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -7,102 +7,99 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include "RefCounting.h"
-#include "Memory.h"
-#include "Allocator.h"
+#include <WinSock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
-using TL = TypeList<class Player, class Mage, class Knight, class Archer>;
-
-class Player
-{
-public:
-	Player()
-	{
-		INIT_TL(Player);
-	}
-	virtual ~Player() {}
-
-	DECLARE_TL
-};
-
-class Knight : public Player
-{
-public:
-	Knight() { INIT_TL(Knight); }
-};
-
-class Mage : public Player
-{
-
-public:
-	Mage() { INIT_TL(Mage); }
-};
-
-class Archer : public Player
-{
-
-public:
-	Archer() { INIT_TL(Archer) }
-};
-
-class Dog
-{
-
-};
 
 int main()
 {
-	//TypeList<Mage, Knight>::Head whoAMI;
-	//TypeList<Mage, Knight>::Tail whoAMI2;
-
-	//TypeList<Mage, TypeList<Knight, Archer>>::Head whoAMI3;
-	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Head whoAMI4;
-	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Tail whoAMI5;
-
-	//int32 len1 = Length<TypeList<Mage, Knight>>::value;
-	//int32 len2 = Length<TypeList<Mage, Knight, Archer>>::value;
-	//
-	//
-	//// 3*3 
-	//TypeAt<TL, 0>::Result whoAMI6;
-	//TypeAt<TL, 1>::Result whoAMI7;
-	//TypeAt<TL, 2>::Result whoAMI8;
-
-	//int32 index1 = IndexOf<TL, Mage>::value;
-	//int32 index2 = IndexOf<TL, Archer>::value;
-	//int32 index3 = IndexOf<TL, Dog>::value;
-
-	//bool canConvert1 = Conversion<Player, Knight>::exists;
-	//bool canConvert2 = Conversion<Knight, Player>::exists;
-	//bool canConvert3 = Conversion<Knight, Dog>::exists;
-
-	/*{
-		Player* player = new Knight();
-
-		bool canCast = CanCast<Knight*>(player);
-		Knight* knight = TypeCast<Knight*>(player);
+    // WinSock 초기화 (ws2_32 라이브러리 초기화)
+    // 관련 정보가 wsaData에 채워짐
+    WSAData wasData;
+    if (::WSAStartup(MAKEWORD(2, 2), &wasData) != 0)
+        return 0;
 
 
-		delete player;
-	}*/
+    // ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
+    // type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+    // protocol : 0
+    // return : descriptor
+    SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+    // SOCKET 타입 자체가 Int형이다. 해당 번호에 해당하는 소켓을 이용하라고 OS에 요청하는거.
 
-	{
-		shared_ptr<Player> player = MakeShared<Knight>();
+    if (listenSocket == INVALID_SOCKET) // Socket생성 실패시
+    {
+        int32 errCode = ::WSAGetLastError();
+        cout << "Socket ErrorCode : " << errCode << endl;
+        return 0;
+    }
 
-		shared_ptr<Archer> archer = TypeCast<Archer>(player);
-		bool canCast = CanCast<Mage>(player);
 
-	}
+    // 서버 주소 설정 (IP주소 + 포트)
+    SOCKADDR_IN serverAddr; //IPv4
+    ::memset(&serverAddr, 0, sizeof(serverAddr)); // SOCKADDR_IN 구조체변수 값을 0으로 초기화
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); // 니가 알아서 골라달라는 의미.
+    // 주소를 하나로 지정하면 그 주소로만 되지만, INADDR_ANY로 하면 LoopBack주소도 되고 ipconfig에 나오는 주소로도 연결가능.
+    serverAddr.sin_port = ::htons(7777); // port설정. 서버쪽에서 열어놓은 포트 번호로 지정되어야함. 
 
-	for (int32 i = 0; i < 5; i++)
-	{
-		GThreadManager->Launch([]()
-			{
-				while (true)
-				{
-				}
-			});
-	}
+    
 
-	GThreadManager->Join();
+    // 안내원 폰 개통! 식당의 대표 번호
+    // 소켓에다가 주소를 묶어주는 것, 첫번째 파라미터인 소켓에 두번재 파라미터인 서버 주소를 연동시키는 것임.
+    if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+    {
+        int32 errCode = ::WSAGetLastError();
+        cout << "Bind ErrorCode : " << errCode << endl;
+        return 0;
+    }
+
+    // 영업 시작 -> 입장가능
+    // 두번째 파라미터인 backlog는 대기열 같은 거임. 꽉찬 경우에 대기열의 최대한도라고 볼 수 있음. 
+    // 이걸 초과하는 애들은 바로 끊어버리는 거지.
+    if (::listen(listenSocket, 10) == SOCKET_ERROR)
+    {
+        int32 errCode = ::WSAGetLastError();
+        cout << "Listen ErrorCode : " << errCode << endl;
+        return 0;
+    }
+
+
+
+    /*----------------------------------------------------------------------
+        여기까지 왔으면 클라에서 connect를 해서 붙는다면, 처리할 준비가 된 것이다.
+    ----------------------------------------------------------------------*/
+
+    while (true)
+    {
+        SOCKADDR_IN clientAddr; //IPv4
+        ::memset(&clientAddr, 0, sizeof(clientAddr));
+        int32 addrlen = sizeof(clientAddr);
+
+        // 두번째 파라미터의 SOCKADDR은 연결되는 클라리언트의 SOCKET 주소이다.
+        /*
+            accept함수는 SOCKET을 리턴하는데, 앞으로 클라이언트와 데이터 통신을 할때는 이 소켓을 이용하면 된다.
+            위에서 사용한 리슨소켓은 클라이언트와 연결해서 새로운 소켓을 받을 수 있도록 문지기 역할을 하는 거지.
+        */
+        SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrlen);
+        if (clientSocket == INVALID_SOCKET)
+        {
+            int32 errCode = ::WSAGetLastError();
+            cout << "Accept ErrorCode : " << errCode << endl;
+            return 0;
+        }
+
+        // 손님 입장 -> 클라 연결됨
+        char ipAddress[16];
+        ::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+        cout << "Client Connected! IP = " << ipAddress << endl;
+
+    }
+
+
+
+    // WinSock 종료
+    ::WSACleanup();
 }
